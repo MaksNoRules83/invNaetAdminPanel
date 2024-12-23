4eba236adc47d5327e6448f2a778840e2bdffde2
MaksSadkov83 <maksraptor543@gmail.com>
2024-12-23 17:00:52 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,8 +1,32 @@
 from flask import Flask
+from flask import render_template, url_for
+import sqlite3
 
 app = Flask(__name__)
 
+db = sqlite3.connect("invNAET.db")
+cursor = db.cursor()
 
 @app.route("/")
 def index():
-    return "<p>Hello world!</p>"
+    return render_template("login.html")
+
+@app.route('/adminpanel/users')
+def adminpanel():
+    return render_template("users.html")
+
+@app.route('/adminpanel/inventories')
+def inventories():
+    return render_template('inventories.html')
+
+@app.route('/adminpanel/cabinets')
+def cabinets():
+    return render_template('cabinets.html')
+
+@app.route('/adminpanel/profile')
+def profile():
+    return render_template('profile.html')
+
+
+if __name__ == "__main__":
+    app.run()
